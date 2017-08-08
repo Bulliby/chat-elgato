81f7405f21d31a12c7fcd83ac5800355efd0b397
Guillaume Wells <wellsguillaume@gmail.com>
2017-08-08 21:55:54 +0200

    feat(master): bundle install

--- src/Bulliby/ChatBundle/Controller/DefaultController.php
+++ src/Bulliby/ChatBundle/Controller/DefaultController.php
@@ -8,10 +8,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('BullibyChatBundle:Default:index.html.twig');
+        return $this->render('BullibyChatBundle:Chat:render.html.twig');
     }
 }
